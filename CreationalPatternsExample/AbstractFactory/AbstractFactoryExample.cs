dd21569e28102396df2793c8a1766c8372e773cf
alexcho0608 <alexstefanov06@gmail.com>
2015-09-13 21:28:57 +0300

    haha
    haha

---
+++ CreationalPatternsExample/AbstractFactory/AbstractFactoryExample.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+
+    abstract class GuiFactory
+    {
+        public static GuiFactory getFactory(int code)
+        {
+            int sys = code;
+            if (sys == 0)
+            {
+                return (new WinFactory());
+            }
+            else
+            {
+                return (new OSXFactory());
+            }
+        }
+        public abstract Button createButton();
+    }
+
+    class WinFactory : GuiFactory
+    {
+        public override Button createButton()
+        {
+            return (new WinButton());
+        }
+    }
+
+    class OSXFactory : GuiFactory
+    {
+        public override Button createButton()
+        {
+            return (new OSXButton());
+        }
+    }
+
+    abstract class Button
+    {
+        public string caption;
+        public abstract void paint();
+    }
+
+    class WinButton : Button
+    {
+        public override void paint()
+        {
+            Console.WriteLine("I'm a WinButton: " + caption);
+        }
+    }
+
+    class OSXButton : Button
+    {
+        public override void paint()
+        {
+            Console.WriteLine("I'm a OSXButton: " + caption);
+        }
+    }
+
+    class AbstractFactoryExample
+    {
+        static void Main(string[] args)
+        {
+            GuiFactory aFactory = GuiFactory.getFactory(0);
+            Button aButton = aFactory.createButton();
+            aButton.caption = "Play";
+            aButton.paint();
+        }
+        //output is
+        //I'm a WinButton: Play
+        //or
+        //I'm a OSXButton: Play
+    }
+}
---
+++ CreationalPatternsExample/MethodFactory/MethodFactoryExample.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodFactory
+{
+    class MethodFactoryExample
+    {
+        static void Main()
+        {
+            // An array of creators
+            Creator[] creators = new Creator[2];
+            creators[0] = new ConcreteCreatorA();
+            creators[1] = new ConcreteCreatorB();
+
+            // Iterate over creators and create products
+            foreach (Creator creator in creators)
+            {
+                Product product = creator.FactoryMethod();
+                Console.WriteLine("Created {0}",
+                  product.GetType().Name);
+            }
+
+            // Wait for user
+            Console.Read();
+        }
+    }
+
+    // "Product"
+
+    abstract class Product
+    {
+    }
+
+    // "ConcreteProductA"
+
+    class ConcreteProductA : Product
+    {
+    }
+
+    // "ConcreteProductB"
+
+    class ConcreteProductB : Product
+    {
+    }
+
+    // "Creator"
+
+    abstract class Creator
+    {
+        public abstract Product FactoryMethod();
+    }
+
+    // "ConcreteCreatorA"
+
+    class ConcreteCreatorA : Creator
+    {
+        public override Product FactoryMethod()
+        {
+            return new ConcreteProductA();
+        }
+    }
+
+    // "ConcreteCreatorB"
+
+    class ConcreteCreatorB : Creator
+    {
+        public override Product FactoryMethod()
+        {
+            return new ConcreteProductB();
+        }
+    }
+}
